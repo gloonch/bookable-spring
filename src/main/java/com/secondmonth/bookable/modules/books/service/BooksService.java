8f00ce2aaa805cc298a3d0b6a87774a66a025737
mahdi hadian <mahdihadian@hotmail.com>
2021-06-23 12:03:30 +0430

    Controllers: before REST

---
+++ src/main/java/com/secondmonth/bookable/modules/books/service/BooksService.java
@@ -0,0 +1,23 @@
+package com.secondmonth.bookable.modules.books.service;
+
+import com.secondmonth.bookable.modules.books.model.Books;
+import com.secondmonth.bookable.modules.books.repository.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BooksService {
+
+    BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Books> findAll() {
+        return booksRepository.findAll();
+    }
+}
